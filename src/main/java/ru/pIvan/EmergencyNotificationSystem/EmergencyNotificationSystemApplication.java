5f6749441d3baf4d5f242cdd24ef14dee8ab77b3
ivant <ivantorsun2004@gmail.com>
2024-03-10 12:57:27 +0300

    Initial commit

---
+++ src/main/java/ru/pIvan/EmergencyNotificationSystem/EmergencyNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.pIvan.EmergencyNotificationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyNotificationSystemApplication.class, args);
+	}
+
+}
